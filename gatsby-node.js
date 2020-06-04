03547aa6668cbfaa84faf1408d1f335704c6c5db
suprabha <suprabha.suprabha@go-jek.com>
2020-06-04 21:42:26 +0530

    fix(post): showing post body :octocat:

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,15 +16,16 @@ exports.createPages = async ({actions, graphql, reporter}) => {
     if(result.errors) {
         reporter.panic("failed to craete post", result.errors)
     }
-
+    // if there is no error then getting nodes into posts variable
     const posts = result.data.allMdx.nodes
-
+    
+    // need to iterarte through the posts so using forEach here
     posts.forEach(post => {
         actions.createPage({
             path: post.frontmatter.slug,
             component: require.resolve("./src/templates/post/post.js"),
             context: {
-                slug: `/${post.frontmatter.slug}`
+                slug: post.frontmatter.slug
             }
         });
     });
--- src/templates/post/post.js
+++ src/templates/post/post.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import {graphql} from 'gatsby'
 import {Link} from 'gatsby'
+import {MDXRenderer} from 'gatsby-plugin-mdx'
 import './post.scss'
 import Layout from '../../components/layout'
 
+export const query = graphql`
+    query($slug: String!) {
+        mdx(frontmatter: {slug: {eq: $slug}}) {
+            frontmatter {
+                title
+                author
+            }
+            body
+        }
+    }
+`
 
-const PostTemplate = () => (
+const PostTemplate = ({data: {mdx: post }}) => (
     <Layout className='post-wrapper'>
-        <h1>Post Title</h1>
-        <p className='author'>Posted by author</p>
-        <p>Post body</p>
-        <p>Post body</p>
+        <h1>{post.frontmatter.title}</h1>
+        <p className='author'>Posted by {post.frontmatter.author}</p>
+        <MDXRenderer>{post.body}</MDXRenderer>
+        {/* <pre>{JSON.stringify(pageContext, null, 2)}</pre> */}
         <Link to="/">&larr; back to all posts</Link>
     </Layout>
 )
