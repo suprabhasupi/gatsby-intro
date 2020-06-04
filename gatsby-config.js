4de51b9846282a9ab3e541d120fb754ad01666dc
suprabha <suprabha.suprabha@go-jek.com>
2020-06-04 19:05:08 +0530

    feat(MDX): added mdx to pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
         title: 'Gatsby Workshop',
         description: 'Learn Gatsby From scratch!'
     },
-    plugins: ['gatsby-plugin-sass', 'gatsby-plugin-react-helmet']
+    plugins: ['gatsby-plugin-sass', 
+            'gatsby-plugin-react-helmet',
+            {
+                resolve: 'gatsby-plugin-mdx',
+                options: {
+                    defaultLayouts: {
+                        default: require.resolve('./src/components/layout')
+                    }
+                }
+            }
+            ]
 }
