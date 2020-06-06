de4c17a1a8ae22e3eed111d82cf8069d1d5775d1
suprabha <suprabha.suprabha@go-jek.com>
2020-06-07 03:18:04 +0530

    feat(instagram): added instagram posts to homepage

--- src/components/instagram/instagram.js
+++ src/components/instagram/instagram.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Image from 'gatsby-image'
+import useInstagram from '../../hooks/use-instagram'
 import './instagram.scss'
 
 const Instagram = () => {
+    const instaPics = useInstagram();
+    const {username} = instaPics[0];
     return (
        <>
-        <h3> Instagram Post from @karan verma</h3>
-        <div>
-            {/* all post */}
+        <h3> Instagram Post from @{username}</h3>
+        <div className='insta-wrapper'>
+            {/* we have done spread in useInstagram hook, so fluid is on top */}
+            {instaPics.map(pic => (
+                 <a href={`https://www.instagram.com/p/${pic.id}/`} className='instagram_pic' target='_blank'>
+                    <Image className='instag-img' fluid={pic.fluid} alt={pic.caption} />
+                 </a>
+            ))}
         </div>
-        <a href='https://www.instagram.com/karanisverma/'>See more on Instagram &rarr;</a>
+        <a href={`https://www.instagram.com/${username}`}>See more on Instagram &rarr;</a>
        </>
     )
 }
